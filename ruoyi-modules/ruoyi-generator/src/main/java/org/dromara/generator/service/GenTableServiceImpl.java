64d574cf06245c6712a3e5a3e73b9f345e446736
AprilWind <2100166581@qq.com>
2024-12-09 11:20:05 +0800

    update 优化代码生成排序问题

--- ruoyi-modules/ruoyi-generator/src/main/java/org/dromara/generator/service/GenTableServiceImpl.java
+++ ruoyi-modules/ruoyi-generator/src/main/java/org/dromara/generator/service/GenTableServiceImpl.java
@@ -106,7 +106,7 @@ public class GenTableServiceImpl implements IGenTableService {
             .like(StringUtils.isNotBlank(genTable.getTableComment()), "lower(table_comment)", StringUtils.lowerCase(genTable.getTableComment()))
             .between(params.get("beginTime") != null && params.get("endTime") != null,
                 "create_time", params.get("beginTime"), params.get("endTime"))
-            .orderByAsc("table_id");
+            .orderByDesc("update_time", "create_time");
         return wrapper;
     }
 
@@ -165,7 +165,8 @@ public class GenTableServiceImpl implements IGenTableService {
                 gen.setCreateTime(x.getCreateTime());
                 gen.setUpdateTime(x.getUpdateTime());
                 return gen;
-            }).toList();
+            }).sorted(Comparator.comparing(GenTable::getCreateTime).reversed())
+            .toList();
 
         IPage<GenTable> page = pageQuery.build();
         page.setTotal(tables.size());
@@ -274,6 +275,7 @@ public class GenTableServiceImpl implements IGenTableService {
                 String tableName = table.getTableName();
                 GenUtils.initTable(table);
                 table.setDataName(dataName);
+                table.setCreateTime(new Date());
                 int row = baseMapper.insert(table);
                 if (row > 0) {
                     // 保存列信息
