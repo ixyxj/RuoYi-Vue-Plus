4598d4d843662a9274cb9b42b9abb5fa4274b70a
疯狂的狮子Li <15040126243@163.com>
2024-12-11 15:31:17 +0800

    update 优化 部门树增加禁用标志位

--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/service/impl/SysDeptServiceImpl.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/service/impl/SysDeptServiceImpl.java
@@ -110,7 +110,8 @@ public class SysDeptServiceImpl implements ISysDeptService, DeptService {
                     tree.setId(dept.getDeptId())
                         .setParentId(dept.getParentId())
                         .setName(dept.getDeptName())
-                        .setWeight(dept.getOrderNum()));
+                        .setWeight(dept.getOrderNum())
+                        .putExtra("disabled", SystemConstants.DISABLE.equals(dept.getStatus())));
                 Tree<Long> tree = trees.stream().filter(it -> it.getId().longValue() == d.getDeptId()).findFirst().get();
                 treeList.add(tree);
             }
