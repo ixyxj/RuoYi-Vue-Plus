2d9930439673c5ae45f75650d0669b88c143d35d
疯狂的狮子Li <15040126243@163.com>
2025-01-22 11:19:51 +0800

    update 优化 WorkflowService 增加获取流程变量方法

--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/service/impl/WorkflowServiceImpl.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/service/impl/WorkflowServiceImpl.java
@@ -79,6 +79,16 @@ public class WorkflowServiceImpl implements WorkflowService {
     }
 
     /**
+     * 获取流程变量
+     *
+     * @param instanceId 流程实例id
+     */
+    @Override
+    public Map<String, Object> instanceVariable(Long instanceId) {
+        return flwInstanceService.instanceVariable(instanceId);
+    }
+
+    /**
      * 按照业务id查询流程实例id
      *
      * @param businessId 业务id
