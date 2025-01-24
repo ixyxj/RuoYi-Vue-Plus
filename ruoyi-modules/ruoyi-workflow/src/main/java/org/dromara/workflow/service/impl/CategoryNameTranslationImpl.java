ffe8b16ff3f793cba1e05daf66ec6df47cf93a4f
疯狂的狮子Li <15040126243@163.com>
2025-01-24 12:17:36 +0800

    fix 修复 翻译报错问题

--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/service/impl/CategoryNameTranslationImpl.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/service/impl/CategoryNameTranslationImpl.java
@@ -1,5 +1,6 @@
 package org.dromara.workflow.service.impl;
 
+import cn.hutool.core.convert.Convert;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.dromara.common.translation.annotation.TranslationType;
@@ -25,9 +26,12 @@ public class CategoryNameTranslationImpl implements TranslationInterface<String>
 
     @Override
     public String translation(Object key, String other) {
+        Long id = null;
         if (key instanceof String categoryId) {
-            return flwCategoryService.selectCategoryNameById(categoryId);
+            id = Convert.toLong(categoryId);
+        } else if (key instanceof Long categoryId) {
+            id = categoryId;
         }
-        return null;
+        return flwCategoryService.selectCategoryNameById(id);
     }
 }
